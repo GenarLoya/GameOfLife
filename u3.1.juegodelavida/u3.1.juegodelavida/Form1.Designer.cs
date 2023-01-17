0f28a4e1041242cff0d72d5c480844004152bf5c
Genaro Loya Dour <genarodour2104@gmail.com>
2023-01-17 14:10:51 -0600

    Font code

---
+++ u3.1.juegodelavida/u3.1.juegodelavida/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace u3._1.juegodelavida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reiniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 510);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pausar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 484);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Iniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(223, 513);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(59, 20);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Izquierda";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(288, 513);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 20);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Abajo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(352, 513);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 20);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Derecha";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(288, 482);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(58, 25);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Arriba";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(223, 539);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(189, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Dibujar";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(202, 544);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(420, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 65);
+            this.label1.TabIndex = 10;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 574);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ u3.1.juegodelavida/u3.1.juegodelavida/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using GlobalKeyboardHooker;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace u3._1.juegodelavida
+{
+    public partial class Form1 : Form
+    {
+
+        int lenght = 40;
+        int lenghtpixel = 10;
+        int[,] golife;
+        int[,] golifecopy;
+        int[,] neighbor;
+        int keyx = 0, keyy = 0;
+        int mode = 0, fill = 0;
+        GlobalKeyboardHook gkh = new GlobalKeyboardHook();
+
+        public Form1()
+        {
+            InitializeComponent();
+            golife = new int[lenght, lenght];
+            golifecopy = new int[lenght, lenght];
+            neighbor = new int[3, 3];
+            neighbor[1, 1] = -2;
+            golife[2, 2] = 1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < lenght; j++)
+                {
+                    golife[i, j] = 0;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Golife();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            mode = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            mode = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Printgolife()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < lenght; j++)
+                {
+                    if (golife[i, j] == 0) Printpixel(bmp, i, j, Color.White);
+                    else if (golife[i, j] == 1) Printpixel(bmp, i, j, Color.Black);
+
+                    if (keyx == i && keyy == j)
+                    {
+                        Printpixel(bmp, i, j, Color.Red);
+                    }
+                }
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void Printpixel(Bitmap bmp, int x, int y, Color color)
+        {
+            for (int i = 0; i < lenghtpixel; i++)
+            {
+                for (int j = 0; j < lenghtpixel; j++)
+                {
+                    bmp.SetPixel(i + (x * lenghtpixel), j + (y * lenghtpixel), color);
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                if (checkBox1.Checked == true) fill = 0;
+                else fill = 1;
+
+                golife[keyx, keyy] = fill; ;
+
+                keyy--;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                if (checkBox1.Checked == true) fill = 0;
+                else fill = 1;
+
+                golife[keyx, keyy] = fill; ;
+
+                keyy++;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                if (checkBox1.Checked == true) fill = 0;
+                else fill = 1;
+
+                golife[keyx, keyy] = fill; ;
+
+                keyx--;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                if (checkBox1.Checked == true) fill = 0;
+                else fill = 1;
+
+                golife[keyx, keyy] = fill; ;
+
+                keyx++;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Printgolife();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (mode == 0)
+            {
+                if (golife[keyx, keyy] == 1) fill = 0;
+                else fill = 1;
+
+                golife[keyx, keyy] = fill;
+            }
+        }
+
+        private void Golife()
+        {
+            int negbhorcount = 0, g = 1;
+
+            for (; ; )
+            {
+
+                if (mode == 0) Console.WriteLine("MODO: Pausa");
+                else if (mode == 1) Console.WriteLine("MODO: Play");
+
+                switch (mode)
+                {
+                    case 0:
+
+                        break;
+
+                    case 1:
+
+                        for (int i = 0; i < lenght; i++)
+                        {
+                            for (int j = 0; j < lenght; j++)
+                            {
+                                //Console.WriteLine(Convert.ToString(i) +", " + (Convert.ToString(j)));
+
+                                if (i - 1 >= 0 && (j - 1) >= 0)
+                                {
+                                    neighbor[0, 0] = golife[i - 1, j - 1];
+                                }
+                                else
+                                {
+                                    neighbor[0, 0] = -1;
+                                }
+
+                                if (i + 1 < lenght && (j + 1) < lenght)
+                                {
+                                    neighbor[2, 2] = golife[i + 1, j + 1];
+                                }
+                                else
+                                {
+                                    neighbor[2, 2] = -1;
+                                }
+
+                                if (i - 1 >= 0 && (j + 1) < lenght)
+                                {
+                                    neighbor[0, 2] = golife[i - 1, j + 1];
+                                }
+                                else
+                                {
+                                    neighbor[0, 2] = -1;
+                                }
+
+                                if (i + 1 < lenght && (j - 1) >= 0)
+                                {
+                                    neighbor[2, 0] = golife[i + 1, j - 1];
+                                }
+                                else
+                                {
+                                    neighbor[2, 0] = -1;
+                                }
+
+                                if (i - 1 >= 0 && (j >= 0 && j < lenght))
+                                {
+                                    neighbor[0, 1] = golife[i - 1, j];
+                                }
+                                else
+                                {
+                                    neighbor[0, 1] = -1;
+                                }
+
+                                if (i + 1 < lenght && (j >= 0 && j < lenght))
+                                {
+                                    neighbor[2, 1] = golife[i + 1, j];
+                                }
+                                else
+                                {
+                                    neighbor[2, 1] = -1;
+                                }
+
+                                if (j - 1 >= 0 && (i >= 0 && i < lenght))
+                                {
+                                    neighbor[1, 0] = golife[i, j - 1];
+                                }
+                                else
+                                {
+                                    neighbor[1, 0] = -1;
+                                }
+
+                                if (j + 1 < lenght && (i >= 0 && i < lenght))
+                                {
+                                    neighbor[1, 2] = golife[i, j + 1];
+                                }
+                                else
+                                {
+                                    neighbor[1, 2] = -1;
+                                }
+
+                                for (int k = 0; k < 3; k++)
+                                {
+                                    for (int l = 0; l < 3; l++)
+                                    {
+                                        if (neighbor[k, l] == 1)
+                                        {
+                                            negbhorcount++;
+                                        }
+                                    }
+                                }
+
+                                if (golife[i, j] == 0)
+                                {
+                                    if (negbhorcount == 3) golifecopy[i, j] = 1;
+                                    else if (negbhorcount < 3 || negbhorcount > 3) golifecopy[i, j] = 0;
+                                }
+                                else if (golife[i, j] == 1)
+                                {
+                                    if (negbhorcount == 3 || negbhorcount == 2) golifecopy[i, j] = 1;
+                                    if (negbhorcount > 3) golifecopy[i, j] = 0;
+                                    else if (negbhorcount < 2) golifecopy[i, j] = 0;
+                                }
+
+                                negbhorcount = 0;
+                            }
+                        }
+
+                        golife = golifecopy;
+
+                        int[,] golifereinit = new int[lenght, lenght];
+
+                        golifecopy = golifereinit;
+
+                        Printgolife();
+
+                        break;
+                }
+
+                if (g == 1) break;
+
+            }
+        }
+
+        private void Keydetect()
+        {
+            
+        }
+    }
+}
